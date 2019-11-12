06433c8b511f69858fb7efdf8bc4184c04bd94ee
Fagner Candido <fsouzacandido@gmail.com>
2019-11-12 14:24:24 +0000

    first commit

---
+++ Main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/info', methods = ['GET'])
+def info():
+    return 'This a simple endpoint to test and provides information about things.'
+
+@app.route('/insert', methods = ['POST'])
+def insert():
+    return 'This a simple endpoint to test insert data.'
+
+if __name__ == '__main__':
+    app.run(debug=True)
